3e2ffefa4360bc7af8d17e34573419309b2cf0d3
Marwan Elsayed <marwanellsayed@hotmail.com>
2024-02-19 03:55:57 +0200

    fix issue 1

--- app/ui/components/OnAirTvSlider.tsx
+++ app/ui/components/OnAirTvSlider.tsx
@@ -1,6 +1,5 @@
 "use client";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
+
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import mediaPlaceHolder from "@/public/mediaplaceholder.png";
